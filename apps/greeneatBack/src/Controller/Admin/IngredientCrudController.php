1ecd8346375598c905bd3856cb3fe94842eb3566
CRaph0705 <raphaelcochrane@hotmail.fr>
2023-01-24 23:55:22 +0100

    index modified admin dashboard

--- apps/greeneatBack/src/Controller/Admin/IngredientCrudController.php
+++ apps/greeneatBack/src/Controller/Admin/IngredientCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Ingredient;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class IngredientCrudController extends AbstractCrudController
 {
@@ -12,14 +15,20 @@ class IngredientCrudController extends AbstractCrudController
         return Ingredient::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('name'),
+            TextEditorField::new('ratio_co2'),
+            TextEditorField::new('ratio_h2o'),
+            TextEditorField::new('nutriscore'),
+            TextEditorField::new('season_from'),
+            TextEditorField::new('season_to'),
+            TextEditorField::new('ecoscore'),
+            TextEditorField::new('ingredientImages'),
         ];
     }
-    */
+    
 }
