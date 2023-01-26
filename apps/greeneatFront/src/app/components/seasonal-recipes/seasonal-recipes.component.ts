ac73f2b1b35663fd89dcf41743dcd0d1c9cabd9f
Orcrax <matthieu.rebate@gmail.com>
2023-01-26 16:36:19 +0100

    working on random recipes

--- apps/greeneatFront/src/app/components/seasonal-recipes/seasonal-recipes.component.ts
+++ apps/greeneatFront/src/app/components/seasonal-recipes/seasonal-recipes.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiSpoonacularService } from 'src/app/services/api-spoonacular.service';
+import { ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-seasonal-recipes',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./seasonal-recipes.component.scss']
 })
 export class SeasonalRecipesComponent {
+  title?: string;
+  image?: string;
+  searchInput = '';
+  idRecipe = 1;
+  titleResult?: string;
+  imageResult?: string;
+  recipes?: any;
+  ingredients?: any;
+  imageRecipeUrl = 'https://spoonacular.com/recipeImages/';
 
+  constructor(private apiSpoonService: ApiSpoonacularService) {
+  }
+
+  ngOnInit(): void {
+    this.apiSpoonService.getApiSpoonacularById(this.idRecipe).subscribe((data)=>{
+      console.log(data);
+      this.title = data.title;
+      this.image = data.image;
+      this.ingredients = data.extendedIngredients;
+    });
+  }
+
+  onSearchRecipe() {
+    this.apiSpoonService.getApiSpoonacularByName(this.searchInput).subscribe((data)=>{
+      this.recipes = data.results;
+      // console.log(this.recipes);
+    });
+  }
 }
