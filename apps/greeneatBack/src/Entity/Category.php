0db7aefdcc4a71df6d38c5441b1a04b9100ce100
CRaph0705 <raphaelcochrane@hotmail.fr>
2023-02-02 00:27:19 +0100

    categories routes reduced to get only on api plateform

--- apps/greeneatBack/src/Entity/Category.php
+++ apps/greeneatBack/src/Entity/Category.php
@@ -3,13 +3,22 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+    ],
+)
+]
 class Category
 {
     #[ORM\Id]
