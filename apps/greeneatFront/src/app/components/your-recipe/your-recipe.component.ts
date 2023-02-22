d8e90e6b49a50016ec28510f4e40aa93a9af6c0c
Nicotinii <nml.lefevre@gmail.com>
2023-02-22 17:06:21 +0100

    Your-recipe-score

--- apps/greeneatFront/src/app/components/your-recipe/your-recipe.component.ts
+++ apps/greeneatFront/src/app/components/your-recipe/your-recipe.component.ts
@@ -7,5 +7,35 @@ import { Component } from '@angular/core';
 })
 
 export class YourRecipeComponent {
+  public greenscore: number = 100;
+  public greenscorecolor: any =this.ColorScore(this.greenscore);
+  public Rscore: number = 0;
+  public Gscore: number = 0;
+  public Bscore: number = 0;
+
+
+  public ColorScoreR(score: number) {
+    this.Rscore = score + 225 - score * 2;
+    return this.Rscore;
+  }
+
+  public ColorScoreG(score: number) {
+    this.Gscore = score + score;
+    return this.Gscore;
+  }
+
+  public ColorScoreB(score: number) {
+    this.Bscore = 0;
+    return this.Bscore;
+  }
+
+  public ColorScore(score: any) {
+    return "color: rgb(" + this. ColorScoreR(score) + ", " + this.ColorScoreG(score) + ", " + this.ColorScoreB(score) + ");";
+
+  }
 
 }
+
+
+
+
